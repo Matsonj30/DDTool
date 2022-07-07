a4ad0ab4bd7c72d69aa5a28fb2f98edc5bd128d0
jwmatson <jwmatson@ualberta.ca>
2022-07-07 17:36:43 -0600

    documentation, financials.py

--- DDtool/tool/views.py
+++ DDtool/tool/views.py
@@ -4,7 +4,10 @@ import requests
 import fundamentalanalysis as fa
 from yahoo_fin.stock_info import *
 from yahoo_fin import news
+from .financials import cashFlow, balanceSheet, incomeStatement #.financials means import from current package
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -17,13 +20,11 @@ def getInfo(request, ticker):
    
     balanceSheetData = balanceSheet()
     incomeStatementData = incomeStatement()
+    cashFlowData = cashFlow()
     newsArticles = getNews()
    
     
-
-
-
-
+    print(cashFlowData)
 
 
     """ GET POST INFO ON GOOGLE FIANANCE
@@ -31,17 +32,7 @@ def getInfo(request, ticker):
         tickerSearched = request.POST.get("searched","") #second one is default
         exchange = request.POST.get("exchanges", "")
       
-
-
-
-        website = requests.get("https://www.google.com/finance/quote/" + str(tickerSearched)+":"+str(exchange))
-        soupWebsite = BeautifulSoup(website.text, "html.parser")
-        findCompanySite = soupWebsite.find_all("a", {"class": "tBHE4e"})
-        print(findCompanySite[-1].decode_contents())
- """
-
-
-
+    """
     return render(request, "search.html",{
         'ticker':tickerSearched,
     })
@@ -57,40 +48,3 @@ def getNews():
 
     return newsArticles
 
-def incomeStatement():
-    annualValues = {}
-
-    annualSheet = get_income_statement("xspa", TTM = True)
-    print(annualSheet)
-
-    annualValues["totalRevenue"] = annualSheet.loc[['totalRevenue']].to_numpy().flatten() 
-    annualValues["COGS"] = annualSheet.loc[['costOfRevenue']].to_numpy().flatten() 
-    annualValues["grossProfit"] = annualSheet.loc[['grossProfit']].to_numpy().flatten()
-    annualValues["totalExpenses"] = annualSheet.loc[['totalOperatingExpenses']].to_numpy().flatten()
-    annualValues["operatingIncome"] = annualSheet.loc[['operatingIncome']].to_numpy().flatten()
-    annualValues["netIncome"] = annualSheet.loc[['netIncome']].to_numpy().flatten()
-
-def balanceSheet():
-    quartAndAnnual = [] #last four quarters if want?
-
-    annualValues = {} #last four years standing
-    annualSheet = get_balance_sheet("xspa")
-
-
-    annualValues["times"] = list(annualSheet) #get X axis titles AKA timeframes
-    annualValues["cash"] = annualSheet.loc[['cash']].to_numpy().flatten() #flatten just turns our silly 2D array to 1D
-    annualValues["totalAssets"] = annualSheet.loc[['totalAssets']].to_numpy().flatten() 
-    annualValues["currentAssets"] = annualSheet.loc[['totalCurrentAssets']].to_numpy().flatten()
-    #put non current assets here
-    annualValues["inventory"] = annualSheet.loc[['inventory']].to_numpy().flatten()
-    annualValues["totalLiabilities"] = annualSheet.loc[['totalLiab']].to_numpy().flatten()
-    annualValues["currentLiabilities"] = annualSheet.loc[['totalCurrentLiabilities']].to_numpy().flatten()
-    
-    #put noncurrent liabilities here
-    
-    return annualValues
-    
-
-
-
-   
